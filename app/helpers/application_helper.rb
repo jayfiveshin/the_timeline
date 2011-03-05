2b4eaad8c7f46275c30fb354923300e68e2fe119
Joonha Shin <shin.joonha@gmail.com>
2011-03-04 20:27:31 -0500

    Added static page, changed root page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Returns title of the webpage
+  def title
+    base_title = "Timeline"
+    if @title.blank?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
 end
