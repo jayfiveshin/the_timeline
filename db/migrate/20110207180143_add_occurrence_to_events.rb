95642126af8f750a929af81287675de22f2a2f71
Joonha Shin <shin.joonha@gmail.com>
2011-03-09 14:24:07 -0500

    Took out a migration file that was causing errors.

--- db/migrate/20110207180143_add_occurrence_to_events.rb
+++
@@ -1,9 +0,0 @@
-class AddOccurrenceToEvents < ActiveRecord::Migration
-  def self.up
-    add_column :events, :occurrence_type, :string
-  end
-
-  def self.down
-    remove_column :events, :occurrence_type
-  end
-end
